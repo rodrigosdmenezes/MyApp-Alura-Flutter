56ac00daf1550f5f987cd2eea68d4cc5f6d35819
rodrigosdmenezes <rodrigosdmenzes@gmail.com>
2024-06-02 11:54:14 -0300

    Adicionando opacidade

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +32,33 @@ class MyApp extends StatelessWidget {
             selectionColor: Colors.white,
           ),
         ),
-        body: ListView(
-          children: [
-            Task('Aprender Flutter',
-                'https://docs.flutter.dev/assets/images/dash/Dashatars.png', 5),
-            Task(
-                'Andar de bike',
-                'https://salvape.com.br/blog/wp-content/uploads/2018/08/230978-reservado-priscila-pfique-por-dentro-dos-beneficios-de-andar-de-bicicleta.jpg',
-                3),
-            Task(
-                'Andar de Skate',
-                'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQro3ggXhRPYNm_1rvLFGrZ7DhCdB8dw4qw_bTTg_xnbZNVSQSzREaT-HGR0PcDFyqY',
-                5),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: Duration(milliseconds: 1000),
+          child: ListView(
+            children: [
+              Task(
+                  'Aprender Flutter',
+                  'https://docs.flutter.dev/assets/images/dash/Dashatars.png',
+                  5),
+              Task(
+                  'Andar de bike',
+                  'https://salvape.com.br/blog/wp-content/uploads/2018/08/230978-reservado-priscila-pfique-por-dentro-dos-beneficios-de-andar-de-bicicleta.jpg',
+                  3),
+              Task(
+                  'Andar de Skate',
+                  'https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQro3ggXhRPYNm_1rvLFGrZ7DhCdB8dw4qw_bTTg_xnbZNVSQSzREaT-HGR0PcDFyqY',
+                  5),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              opacidade = !opacidade;
+            });
+          },
+          child: Icon(Icons.remove_red_eye),
         ),
       ),
     );
